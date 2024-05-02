0cfa73d5c6407a0a2b61106ee3502d956a9c4dba
sri0606 <srirams0606@gmail.com>
2024-05-02 00:24:15 -0400

    added image captioning

--- src/vision.py
+++ src/vision.py
@@ -1,5 +1,16 @@
 
 from .utils.image import Image
+from transformers import VisionEncoderDecoderModel, ViTImageProcessor, AutoTokenizer
+import torch
+from PIL import Image
+
+model = VisionEncoderDecoderModel.from_pretrained("models/gpt2-image-captioning")
+feature_extractor = ViTImageProcessor.from_pretrained("models/gpt2-image-captioning")
+tokenizer = AutoTokenizer.from_pretrained("models/gpt2-image-captioning")
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
 
 class VisionAid:
     """
@@ -62,9 +73,32 @@ class Vision:
 
         return results
 
-    def get_context(self):
+    def get_context(self,image_paths):
         """
         Get the context of the visuals
         """
-        #get the context of the visuals from image analysis
-        return self.process_image()
+        max_length = 16
+        num_beams = 4
+        gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
+
+        visual_context = self.__predict_image_caption(image_paths,gen_kwargs)
+
+        return visual_context
+    
+    def __predict_image_caption(self,image_paths,gen_kwargs):
+        images = []
+        for image_path in image_paths:
+            i_image = Image.open(image_path)
+            if i_image.mode != "RGB":
+                i_image = i_image.convert(mode="RGB")
+
+            images.append(i_image)
+
+        pixel_values = feature_extractor(images=images, return_tensors="pt").pixel_values
+        pixel_values = pixel_values.to(device)
+
+        output_ids = model.generate(pixel_values, **gen_kwargs,pad_token_id=None)
+
+        preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
+        preds = [pred.strip() for pred in preds]
+        return preds
