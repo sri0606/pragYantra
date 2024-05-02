58d4b0d67195497e2056fa299023d1dd88d6d8bb
sri0606 <srirams0606@gmail.com>
2024-05-02 00:22:35 -0400

    minor changes

--- src/hear.py
+++ src/hear.py
@@ -1,10 +1,10 @@
 from whisper_live.client import TranscriptionClient
 client = TranscriptionClient(
-  "127.0.0.1",
-  58142,
+  "localhost",
+  9090,
   lang="en",
   translate=False,
-  model="small",
+  model="base",
   use_vad=False,
 )
 
