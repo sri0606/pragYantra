acbbc18923eb6c8f30216bccc1693487e94beb5b
sri0606 <srirams0606@gmail.com>
2024-05-03 00:03:32 -0400

    Working LiveTranscriber

--- src/hear.py
+++ src/hear.py
@@ -1,11 +1,47 @@
-from whisper_live.client import TranscriptionClient
-client = TranscriptionClient(
-  "localhost",
-  9090,
-  lang="en",
-  translate=False,
-  model="base",
-  use_vad=False,
-)
+# from whisper_live.client import TranscriptionClient
+# client = TranscriptionClient(
+#   "localhost",
+#   9090,
+#   lang="en",
+#   translate=False,
+#   model="base",
+#   use_vad=False,
+# )
 
-client()
+# client()
+
+import threading
+from utils.live_transcribe import live_transcribe
+
+class LiveTranscriber:
+    """
+    LiveTranscriber class to handle live transcription
+    """
+    def __init__(self):
+      self.thread = None
+      self.stop_event = threading.Event()
+
+    def start(self):
+      if self.thread is not None and self.thread.is_alive():
+          print("Transcription is already running")
+          return
+
+      self.thread = threading.Thread(target=live_transcribe, 
+                        kwargs={'model': 'base', 'non_english': False,'stop_event': self.stop_event})
+      self.thread.start()
+      print("Transcribing thread started.")
+
+    def stop(self):
+      if self.thread is None or not self.thread.is_alive():
+          print("Transcription is not running")
+          return
+
+      # Signal the transcription thread to stop
+      self.stop_event.set()
+
+      # Wait for the transcription thread to finish
+      self.thread.join()
+
+      # Reset the stop event so we can start the transcription again
+      self.stop_event.clear()
+      print("Transcribing thread stopped.")
--- src/utils/live_transcribe.py
+++ src/utils/live_transcribe.py
@@ -1,6 +1,4 @@
 #! python3.7
-
-import argparse
 import os
 import numpy as np
 import speech_recognition as sr
@@ -13,24 +11,21 @@ from time import sleep
 from sys import platform
 import json
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--model", default="medium", help="Model to use",
-                        choices=["tiny", "base", "small", "medium", "large"])
-    parser.add_argument("--non_english", action='store_true',
-                        help="Don't use the english model.")
-    parser.add_argument("--energy_threshold", default=1000,
-                        help="Energy level for mic to detect.", type=int)
-    parser.add_argument("--record_timeout", default=2,
-                        help="How real time the recording is in seconds.", type=float)
-    parser.add_argument("--phrase_timeout", default=3,
-                        help="How much empty space between recordings before we "
-                             "consider it a new line in the transcription.", type=float)
-    if 'linux' in platform:
-        parser.add_argument("--default_microphone", default='pulse',
-                            help="Default microphone name for SpeechRecognition. "
-                                 "Run this with 'list' to view available Microphones.", type=str)
-    args = parser.parse_args()
+####  inspired from https://github.com/davabase/whisper_real_time   #####
+def live_transcribe(model="medium", non_english=False, energy_threshold=1000, 
+                    record_timeout=2, phrase_timeout=3, default_microphone='pulse',stop_event=None):
+    """
+    Transcribes audio from the microphone in real time.
+
+    Parameters:
+    model (str): The model to use for transcription. Default is "medium".
+    non_english (bool): Whether to use a non-English model. Default is False.
+    energy_threshold (int): The energy level threshold for the speech recognizer. Default is 1000.
+    record_timeout (int): The maximum length of a single phrase in seconds. Default is 2.
+    phrase_timeout (int): The maximum time to wait for a new phrase in seconds. Default is 3.
+    default_microphone (str): The default microphone to use. Default is 'pulse'.
+    stop_event (threading.Event): An event that will be set when the transcription should stop.
+    """
 
     # The last time a recording was retrieved from the queue.
     phrase_time = None
@@ -38,14 +33,14 @@ def main():
     data_queue = Queue()
     # We use SpeechRecognizer to record our audio because it has a nice feature where it can detect when speech ends.
     recorder = sr.Recognizer()
-    recorder.energy_threshold = args.energy_threshold
+    recorder.energy_threshold = energy_threshold
     # Definitely do this, dynamic energy compensation lowers the energy threshold dramatically to a point where the SpeechRecognizer never stops recording.
     recorder.dynamic_energy_threshold = False
 
     # Important for linux users.
     # Prevents permanent application hang and crash by using the wrong Microphone
     if 'linux' in platform:
-        mic_name = args.default_microphone
+        mic_name = default_microphone
         if not mic_name or mic_name == 'list':
             print("Available microphone devices are: ")
             for index, name in enumerate(sr.Microphone.list_microphone_names()):
@@ -60,13 +55,9 @@ def main():
         source = sr.Microphone(sample_rate=16000)
 
     # Load / Download model
-    model = args.model
-    if args.model != "large" and not args.non_english:
-        model = model + ".en"
-    audio_model = whisper.load_model(model)
-
-    record_timeout = args.record_timeout
-    phrase_timeout = args.phrase_timeout
+    if model != "large" and not non_english:
+            model = model + ".en"
+    audio_model = whisper.load_model(os.path.abspath(f"models/{model}.pt"))
 
     with source:
         recorder.adjust_for_ambient_noise(source)
@@ -94,37 +85,29 @@ def main():
     text = ''
     
     transcriptions = {}
+
     while True:
+        # Check the stop event at the start of each loop iteration
+        if stop_event is not None and stop_event.is_set():
+            break
         try:
             now = datetime.now()
-            # Get the current hour
             current_hour = now.strftime("%H")
 
-            # Pull raw recorded audio from the queue.
             if not data_queue.empty():
                 phrase_complete = False
-                # If enough time has passed between recordings, consider the phrase complete.
-                # Clear the current working audio buffer to start over with the new data.
                 if phrase_time and now - phrase_time > timedelta(seconds=phrase_timeout):
                     phrase_complete = True
-                # This is the last time we received new audio data from the queue.
                 phrase_time = now
                 
-                # Combine audio data from queue
                 audio_data = b''.join(data_queue.queue)
                 data_queue.queue.clear()
                 
-                # Convert in-ram buffer to something the model can use directly without needing a temp file.
-                # Convert data from 16 bit wide integers to floating point with a width of 32 bits.
-                # Clamp the audio stream frequency to a PCM wavelength compatible default of 32768hz max.
                 audio_np = np.frombuffer(audio_data, dtype=np.int16).astype(np.float32) / 32768.0
 
-                # Read the transcription.
                 result = audio_model.transcribe(audio_np, fp16=torch.cuda.is_available())
                 text += result['text'].strip()
 
-                # If we detected a pause between recordings, add a new item to our transcription.
-                # Otherwise edit the existing one.
                 if current_hour not in transcriptions:
                     transcriptions[current_hour] = []
 
@@ -132,16 +115,11 @@ def main():
                     transcriptions[current_hour] = [{now.strftime("%H%M%S"): text}]
                     text = ''
 
-                # If the timer has expired, save the transcriptions to a JSON file
                 if now >= next_save_time:
-                    # Format the date as a string in the format YearMonthDay
                     date_string = now.strftime("%Y%m%d")
-
-                    # Use the date string as the filename
                     filename = f"{date_string}_transcript.json"
                     filepath = os.path.join(transcript_dir_path, filename)
 
-                    # Load the existing data
                     if os.path.exists(filepath):
                         with open(filepath, 'r') as f:
                             if f.read().strip():
@@ -152,40 +130,18 @@ def main():
                     else:
                         existing_data = {}
 
-                    # If the current hour is not in the existing data, add it
                     if current_hour not in existing_data:
                         existing_data[current_hour] = []
 
-                    # Append the new transcriptions to the existing data
                     existing_data[current_hour].extend(transcriptions[current_hour])
 
-                    # Save everything to the JSON file
                     with open(filepath, 'w') as f:
                         json.dump(existing_data, f)
 
-                    # Clear the transcriptions for the current hour
                     transcriptions[current_hour].clear()
 
-                    # Reset the timer
                     next_save_time = now + timedelta(seconds=3)
-
-
-                # Clear the console to reprint the updated transcription.
-                os.system('cls' if os.name=='nt' else 'clear')
-                for line,val in transcriptions.items():
-                    print(line, ": ",val)
-                # Flush stdout.
-                print('', end='', flush=True)
             else:
-                # Infinite loops are bad for processors, must sleep.
                 sleep(0.25)
         except KeyboardInterrupt:
             break
-
-    print("\n\nTranscription:")
-    for line in transcriptions:
-        print(line)
-
-
-if __name__ == "__main__":
-    main()
