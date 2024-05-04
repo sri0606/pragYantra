6312e5fc08c60a88126b9f8ca4b8f0e62b13ce7e
sri0606 <srirams0606@gmail.com>
2024-05-04 00:09:07 -0400

    working live speech

--- src/speech.py
+++ src/speech.py
@@ -1,130 +1,90 @@
-import pyttsx3
-
-class Speech:
-    """
-    Speech class to handle all speech related tasks
-    """
-    def __init__(self):
-        self._speech = None
-
-    def text_to_speech(self, text):
-        raise NotImplementedError("Subclass must implement text_to_speech method")
-
-
-class Pyttsx3Speech(Speech):
-    """
-    A class that provides text-to-speech functionality using the pyttsx3 library.
-
-    Attributes:
-        speech_engine (pyttsx3.Engine): The pyttsx3 speech engine.
-        speech_rate (int): The speech rate in words per minute.
-
-    Methods:
-        text_to_speech(text): Converts the given text to speech.
-        save_to_file(filename, text): Saves the given text to a file.
-        play(): Plays the speech.
-        set_speech_rate(rate): Sets the speech rate to the specified value.
-        get_speech_rate(): Returns the current speech rate.
-        set_speech_voice(voice): Sets the speech voice to the specified value.
-        get_speech_voices(): Returns the available speech voices.
-    """
-
-    def __init__(self):
-        super().__init__()
-        self.speech_engine = pyttsx3.init()
-        self.speech_rate=200
-
-    def speak_instant(self,text:str):
-        self.speech_engine.speak(text)
-
-    def text_to_speech(self, text:str):
-        # Implementation for pyttsx3 goes here
-        self.speech_engine.say(text)
-    
-    def save_to_file(self, filename:str, text:str):
-        self.speech_engine.save_to_file(text, filename)
-
-    def play(self):
-        self.speech_engine.runAndWait()
-
-    def set_speech_rate(self,rate):
-        self.speech_rate=rate
-        self.speech_engine.setProperty('rate', rate)
-
-    def get_speech_rate(self):
-        return self.speech_rate
-    
-    def set_speech_voice(self, voice_id):
-        """
-        Set the voice for the speech engine.
-
-        :param voice_id: The ID of the voice to use.
-        """
-        self.speech_engine.setProperty('voice', voice_id)
-
-
-    def get_speech_voices(self):
-        """
-        Get ids of all voices available on your machine.
-
-        :return: A dictionary where the keys are the indices and the values are the voice objects.
-        """
-        voices = {}
-        for i, voice in enumerate(self.speech_engine.getProperty('voices')):
-            voices[i] = voice.id
-        return voices
-
-    def set_volume(self, volume):
-        """
-        Set the volume of the speech engine. only between 0 and 1
-
-        :param volume: The volume to set.
-        """
-        if not 0 <= volume <= 1:
-            raise ValueError("Volume must be between 0 and 1")
-
-        self.speech_engine.setProperty('volume', volume)
-
-    def get_volume(self):
-        """
-        Get the current volume of the speech engine.
-
-        :return: The current volume.
-        """
-        return self.speech_engine.getProperty('volume')
-
-import requests
+import threading
+from utils.aid_speech import Pyttsx3Speech
+import queue
 import os
-from dotenv import load_dotenv
-load_dotenv()
+import pygame
+import time
+from datetime import datetime
 
-class ElevenLabsSpeech:
-    
+class LiveSpeech:
+    """
+    LiveSpeech class to handle live speech
+    """
+    def __init__(self):
+        pygame.mixer.init()
+        self._speaker = Pyttsx3Speech()
+        self._thread = None
+        self._queue = queue.Queue()
+        self._stop_event = threading.Event()
 
-    def text_to_speech_eleven_labs_api(self, text):
-        """
-        This method converts text to audio
-        """
-        speech_key = os.getenv("SPEECH_KEY")
-        CHUNK_SIZE = 1024
-        url = "https://api.elevenlabs.io/v1/text-to-speech/21m00Tcm4TlvDq8ikWAM"
-        headers = {
-        "Accept": "audio/mpeg",
-        "Content-Type": "application/json",
-        "xi-api-key": speech_key
-        }
+    def _process_speech(self):
+        while not self._stop_event.is_set():
+            try:
+                text = self._queue.get(timeout=1)
+            except queue.Empty:
+                continue
 
-        data = {
-        "text": text,
-        "model_id": "eleven_monolingual_v1",
-        "voice_settings": {
-            "stability": 0.5,
-            "similarity_boost": 0.5
-            }
-        }
+            save_dir = os.path.join(os.getcwd(), "memory_stream/audio_logs/")
+            filename=save_dir+datetime.now().strftime("%Y%m%d%H%M%S")+".wav"
 
-        response = requests.post(url, json=data, headers=headers)
-        with open('output.mp3', 'wb') as f:
-            for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
-                if chunk:
-                    f.write(chunk)
+            # Save the speech to an audio file in a separate thread
+            self._speaker.save_to_file(filename, text)
+            self._play_audio(filename)
+
+    def _play_audio(self, filename):
+        pygame.mixer.music.load(filename)
+        pygame.mixer.music.play()
+
+        while pygame.mixer.music.get_busy():
+            if self._stop_event.is_set():
+                pygame.mixer.music.stop()
+                break
+
+            time.sleep(0.1)
+
+    def start(self):
+        if self._thread is not None and self._thread.is_alive():
+          print("Speech is already running")
+          return
+        
+        self._thread = threading.Thread(target=self._process_speech)
+        self._thread.start()
+
+        print("Speech thread started")
+
+    def speak(self,text):
+        self.shut_up()
+        self._queue.put(text)
+
+    def pause(self):
+        # Pause the audio playback
+        pygame.mixer.music.pause()
+
+    def shut_up(self):
+        # Stop the audio playback
+        pygame.mixer.music.stop()
+
+    def continue_speaking(self):
+        # Resume the audio playback
+        pygame.mixer.music.unpause()
+
+
+    def terminate(self):
+        if self._thread is None or not self._thread.is_alive():
+            return
+        # Stop the audio playback and the speech processing thread
+        pygame.mixer.music.stop()
+
+        # Signal the Speech thread to stop
+        self._stop_event.set()
+
+        # Wait for the Speech thread to finish
+        self._thread.join(timeout=1.0)
+
+        # Reset the stop event so we can start the Speech again
+        self._stop_event.clear()
+
+        print("Speech thread stopped.")
+
+
+
---
+++ src/utils/aid_speech.py
@@ -0,0 +1,288 @@
+import pyttsx3
+import os
+
+class Speech:
+    """
+    Speech class to handle all speech related tasks
+    """
+    def __init__(self):
+        self._speech = None
+
+    def speak_instant(self, text):
+        raise NotImplementedError("Subclass must implement text_to_speech method")
+
+    def save_to_file(self, filename, text):
+        raise NotImplementedError("Subclass must implement save_to_file method")
+
+class Pyttsx3Speech(Speech):
+    """
+    A class that provides text-to-speech functionality using the pyttsx3 library.
+
+    Attributes:
+        speech_engine (pyttsx3.Engine): The pyttsx3 speech engine.
+        speech_rate (int): The speech rate in words per minute.
+
+    Methods:
+        text_to_speech(text): Converts the given text to speech.
+        save_to_file(filename, text): Saves the given text to a file.
+        play(): Plays the speech.
+        set_speech_rate(rate): Sets the speech rate to the specified value.
+        get_speech_rate(): Returns the current speech rate.
+        set_speech_voice(voice): Sets the speech voice to the specified value.
+        get_speech_voices(): Returns the available speech voices.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.speech_engine = pyttsx3.init()
+        self.speech_rate=200
+
+    def speak_instant(self,text:str):
+        self.speech_engine.say(text)
+        self.speech_engine.runAndWait()
+
+    def text_to_speech(self, text:str):
+        # Implementation for pyttsx3 goes here
+        self.speech_engine.say(text)
+    
+    def save_to_file(self, filename:str, text:str):
+        self.speech_engine.save_to_file(text, filename)
+        self.speech_engine.runAndWait()
+
+    def play(self):
+        self.speech_engine.runAndWait()
+
+    def stop(self):
+        self.speech_engine.stop()
+
+    def set_speech_rate(self,rate):
+        self.speech_rate=rate
+        self.speech_engine.setProperty('rate', rate)
+
+    def get_speech_rate(self):
+        return self.speech_rate
+    
+    def set_speech_voice(self, voice_id):
+        """
+        Set the voice for the speech engine.
+
+        :param voice_id: The ID of the voice to use.
+        """
+        self.speech_engine.setProperty('voice', voice_id)
+
+
+    def get_speech_voices(self):
+        """
+        Get ids of all voices available on your machine.
+
+        :return: A dictionary where the keys are the indices and the values are the voice objects.
+        """
+        voices = {}
+        for i, voice in enumerate(self.speech_engine.getProperty('voices')):
+            voices[i] = voice.id
+        return voices
+
+    def set_volume(self, volume):
+        """
+        Set the volume of the speech engine. only between 0 and 1
+
+        :param volume: The volume to set.
+        """
+        if not 0 <= volume <= 1:
+            raise ValueError("Volume must be between 0 and 1")
+
+        self.speech_engine.setProperty('volume', volume)
+
+    def get_volume(self):
+        """
+        Get the current volume of the speech engine.
+
+        :return: The current volume.
+        """
+        return self.speech_engine.getProperty('volume')
+
+import requests
+
+from pydub import AudioSegment
+from pydub.playback import play
+import io
+import time
+from dotenv import load_dotenv
+load_dotenv()
+
+class ElevenLabsSpeech(Speech):
+    """
+    A class that provides text-to-speech functionality using the Eleven Labs API.
+    """
+
+    def __get_response(self, text):
+        """
+        This method converts text to audio using Eleven Labs API.
+        
+        Args:
+            text (str): The text to be converted to audio.
+        
+        Returns:
+            requests.Response: The response object containing the audio data.
+        """
+        speech_key = os.getenv("SPEECH_KEY")
+        CHUNK_SIZE = 1024
+        url = "https://api.elevenlabs.io/v1/text-to-speech/21m00Tcm4TlvDq8ikWAM"
+        headers = {
+            "Accept": "audio/mpeg",
+            "Content-Type": "application/json",
+            "xi-api-key": speech_key
+        }
+
+        data = {
+            "text": text,
+            "model_id": "eleven_monolingual_v1",
+            "voice_settings": {
+                "stability": 0.5,
+                "similarity_boost": 0.5
+            }
+        }
+
+        response = requests.post(url, json=data, headers=headers)
+        return response
+    
+    def speak_instant(self, text):
+        """
+        Converts the given text to audio and plays it instantly.
+        
+        Args:
+            text (str): The text to be converted to audio.
+        """
+        response = self.__get_response(text)
+        audio_data = response.content
+        audio = AudioSegment.from_file(io.BytesIO(audio_data), format="mp3")
+        play(audio)
+        return
+
+    def save_to_file(self, filename, text,format="wav"):
+        """
+        Converts the given text to audio and saves it to a file.
+        
+        Args:
+            filename (str): The name of the file to save the audio to.
+            text (str): The text to be converted to audio.
+        """
+        response = self.__get_response(text)
+        audio_data = response.content
+        audio = AudioSegment.from_file(io.BytesIO(audio_data), format=format)
+        audio.export(filename, format=format)
+        return
+
+from transformers import VitsModel, VitsTokenizer
+import torch
+import numpy as np
+class FacebookMMS(Speech):
+    """
+    FacebookMMS class for text-to-speech using Facebook MMS TTS model.
+    """
+
+    def __init__(self, language="en"):
+        """
+        Constructor for the FacebookMMS class.
+
+        Args:
+            language (str): The language to use for the model. Default is "en".
+        """
+        super().__init__()
+        model_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "models/facebook_mms_tts_eng")
+        try:
+            self.model = VitsModel.from_pretrained(model_dir)
+            self.tokenizer = VitsTokenizer.from_pretrained(model_dir)
+        except Exception as e:
+            print(f"Failed to load Facebook MMS TTS model or tokenizer: {e}")
+            raise
+    
+    def __get_speech_data(self, text):
+        """
+        Generate speech data for the given text.
+
+        Args:
+            text (str): The input text to convert to speech.
+
+        Returns:
+            torch.Tensor: The speech data.
+        """
+        inputs = self.tokenizer(text, return_tensors="pt")
+        input_ids = inputs["input_ids"]
+
+        with torch.no_grad():
+            outputs = self.model(input_ids)
+
+        speech = outputs["waveform"]
+        return speech
+    
+    def speak_instant(self, text):
+        """
+        Generate speech for the given text.
+
+        Args:
+            text (str): The input text to convert to speech.
+
+        Returns:
+            None
+        """
+        speech = self.__get_speech_data(text)
+
+        # Convert the waveform to a NumPy array
+        speech_np = speech.numpy()
+
+        # Convert the waveform to an AudioSegment
+        audio = AudioSegment(
+            speech_np.tobytes(),
+            frame_rate=16000,
+            sample_width=speech_np.dtype.itemsize,
+            channels=1
+        )
+        # Play the audio
+        play(audio)
+    
+    def save_to_file(self, filename, text,format="wav"):
+        """
+        Generate speech for the given text and save it to a file.
+
+        Args:
+            filename (str): The name of the file to save the audio to.
+            text (str): The input text to convert to speech.
+
+        Returns:
+            None
+        """
+        speech = self.__get_speech_data(text)
+
+        # Convert the waveform to a NumPy array and scale it to 16-bit signed integers
+        speech_np = (speech.numpy() * 32767).astype(np.int16)
+        
+        # Convert the waveform to an AudioSegment
+        audio = AudioSegment(
+            speech_np.tobytes(),
+            frame_rate=16000,
+            sample_width=2,  # 16-bit audio
+            channels=1
+        )
+        # Save the audio to a file
+        audio.export(filename, format=format)
+
+if __name__ == "__main__":
+    text="Hey! Long time no see. How's everything going with you? I just got back from a weekend trip, and it was so refreshing.  Did you catch the game last night? It was pretty intense!"
+
+    speech1 = Pyttsx3Speech() 
+    speech2 = ElevenLabsSpeech() 
+    speech3 = FacebookMMS() 
+
+    print("\n\n")
+    start_time = time.time()
+    speech1.speak_instant(text) #for the above text, took 12-13sec
+    print("Pyttsx3Speech: "," %s seconds" % (time.time() - start_time))
+    
+    start_time = time.time()
+    speech2.speak_instant(text) #for the above text, took 13-14sec
+    print("ElevenLabsSpeech: "," %s seconds" % (time.time() - start_time))
+
+    start_time = time.time()
+    speech3.speak_instant(text) #for the above text, took 17-18sec
+    print("FacebookMMS: ","%s seconds" % (time.time() - start_time))
